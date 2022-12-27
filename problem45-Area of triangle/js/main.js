506aa88543cb59369efe47d9b8e05f2b007b13a2
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:37:23 +0530

    adding files.

---
+++ problem45-Area of triangle/js/main.js
@@ -0,0 +1,7 @@
+var basevalue=prompt("enter the value of basevalue");
+basevalue=parseInt(basevalue);
+var heightvalue=prompt("enter the value of heightvalue");
+heightvalue=parseInt(heightvalue);
+var areaoftriangle;
+var areaoftriangle=1/2*basevalue*heightvalue;
+alert(areaoftriangle);
